c121efd17ca686aae368e31e48bd3822f53c0f5b
IamGideonAkpaidang <131524836+IamGideonAkpaidang@users.noreply.github.com>
2026-05-09 23:50:49 +0100

    Fix GitHub Pages path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/hearts-united-ui/',
   server: {
     host: "::",
     port: 8080,
